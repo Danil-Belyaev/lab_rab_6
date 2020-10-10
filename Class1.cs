3e3ada68c87ccfdb11b5b631ea70143574f33fb7
Danil-Belyaev <70798431+Danil-Belyaev@users.noreply.github.com>
2020-10-10 18:49:23 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace КЛАССЫ._ОБЪЕКТНОЕ_МОДЕЛИРОВАНИЕ
+{
+    class Kub
+    {
+        private double a;
+
+        public double V()
+        {
+            return a * a * a;
+        }
+        public double S()
+        {
+            return 6 * a * a;
+        }
+
+        public void Info()
+        {
+            String str =
+                "   *******\n" +
+                " * |    **\n" +
+                "******** *\n" +
+                "*  |___*_*\n" +
+                "* /куб **\n" +
+                "********\n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("Сторона куба = {0:0.00}", a));
+            Console.WriteLine(string.Format("Объем куба = {0:0.00}", V()));
+            Console.WriteLine(string.Format("Площадь куба = {0:0.00}", S()));
+        }
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace КЛАССЫ._ОБЪЕКТНОЕ_МОДЕЛИРОВАНИЕ
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"Kub_output.txt");
+            var new_in = new StreamReader(@"Kub_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+
+            Kub k = new Kub();
+            k.Load();
+            k.Info();
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+            Console.SetIn(save_in);
+            new_in.Close();
+#else
+
+            Console.ReadKey();
+#endif
+        }
+    }
+
+}
